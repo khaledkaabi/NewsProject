1c7ff5dcd07852056594a181aedd68de7248003d
Khaled <khaled.kaabi@esprit.tn>
2019-01-28 16:09:51 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegiterComponent } from './regiter/regiter.component';
+import { LoginComponent } from './login/login.component';
+import { ForgetComponent } from './forget/forget.component';
+import { CardComponent } from './card/card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component:LoginComponent  },
+  { path: 'register', component: RegiterComponent },
+  { path: 'forget', component: ForgetComponent },
+  { path: 'home', component: CardComponent },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegiterComponent } from './regiter/regiter.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SlideBarComponent } from './slide-bar/slide-bar.component';
+import { CardComponent } from './card/card.component';
+import { ForgetComponent } from './forget/forget.component';
+import {HttpClientModule} from '@angular/common/http';
+import { ArticleService } from './article.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegiterComponent,
+    NavBarComponent,
+    SlideBarComponent,
+    CardComponent,
+    ForgetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ArticleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
